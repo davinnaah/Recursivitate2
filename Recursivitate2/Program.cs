4f86db21ac3b44ccbe9b283ada2fcc9bfa1372cc
User <User@DESKTOP-PQKU9A0>
2022-01-13 23:48:47 +0200

    Add project files.

---
+++ Recursivitate2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Recursivitate2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vector = new int[] { 5, 6, 7, 8, 9 };
+            Console.WriteLine(recursiv.Maxim(vector, vector.Length - 1, int.MinValue));
+        }
+    }
+}
---
+++ Recursivitate2/recursiv.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursivitate2
+{
+    class recursiv
+    {
+        public static int Maxim(int[] vector, int n, int maxx)
+        {
+            if (n == 0)
+            {
+                if (maxx >= vector[n])
+                    return maxx;
+                else
+                    return vector[n];
+            }
+            else
+            {
+                if (maxx >= vector[n])
+                    return Maxim(vector, n - 1, maxx);
+                else
+                    return Maxim(vector, n - 1, vector[n]);
+            }
+        }
+    }
+}
